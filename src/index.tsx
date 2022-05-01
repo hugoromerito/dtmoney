02fed66a4541b07d36793c1be234fbc8f4e45af4
Hugo Romerito <hugoxvida@gmail.com>
2022-04-30 22:18:10 -0300

    Conectando API fictícia
    Consumindo API

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,24 @@
 import { createRoot } from 'react-dom/client';
+import {createServer} from 'miragejs'
 import { App } from './App';
+
+createServer({
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return [
+                {
+                    id: 1,
+                    title: 'Transaction 1',
+                    amount: 400,
+                    type: 'deposit',
+                    category: 'Food',
+                    createAt: new Date()
+                }
+            ]
+        })
+    }
+})
 
 createRoot(document.getElementById('root')!).render(<App />);
