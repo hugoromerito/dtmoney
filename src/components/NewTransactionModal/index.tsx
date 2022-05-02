795bf51fe0c97ccbf904e6e57c629149f667a24f
Hugo Romerito <hugoxvida@gmail.com>
2022-05-02 02:45:48 -0300

    Salvando Dados do Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -12,6 +12,15 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+  const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+  } 
+
   return (
     <Modal
       isOpen={isOpen}
@@ -32,11 +41,15 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         
         <input
           placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
         />
         
         <input
           type="number"
           placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -63,6 +76,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
         <input
           placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         />
 
         <button type="submit">
