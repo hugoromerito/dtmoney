f979a40112c527712a2a52570dbf41586f985abb
Hugo Romerito <hugoxvida@gmail.com>
2022-05-03 00:03:44 -0300

    Aplicação atualizada

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,10 +1,9 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
 
 export function TransactionsTable() {
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions();
 
     return (
         <Container>
@@ -30,8 +29,7 @@ export function TransactionsTable() {
                                 </td>
                                 <td>{transaction.category}</td>
                                 <td>{new Intl.DateTimeFormat('pt-BR', {
-                                        dateStyle: 'short',
-                                        timeStyle: 'medium'
+                                        dateStyle: 'short'
                                 }).format(
                                         new Date(transaction.createdAt)
                                     )}
