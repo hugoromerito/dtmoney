c722b23278688347ceb18bb4b4a07cb07e570c70
Hugo Romerito <hugoxvida@gmail.com>
2022-05-02 23:07:25 -0300

    Finalizando inserção
    Finalizando a funcionalidade de inserção de dados na tabela da aplicação

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
 
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    }, []);
+    const { transactions } = useContext(TransactionsContext);
 
     return (
         <Container>
@@ -42,7 +29,10 @@ export function TransactionsTable() {
                                     }).format(transaction.amount)}
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{new Intl.DateTimeFormat('pt-BR').format(
+                                <td>{new Intl.DateTimeFormat('pt-BR', {
+                                        dateStyle: 'short',
+                                        timeStyle: 'medium'
+                                }).format(
                                         new Date(transaction.createdAt)
                                     )}
                                 </td>    
